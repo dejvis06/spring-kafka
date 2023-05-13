b98d83b8386944bce74eb3bc78b7410ba7f1bd77
beqirajd <dejvis.beqiraj@linksmt.it>
2023-05-13 10:21:10 +0200

    create domain entities

---
+++ producer/src/main/java/com/example/domain/Book.java
@@ -0,0 +1,17 @@
+package com.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Book {
+
+    private Integer bookId;
+    private String bookName;
+    private String bookAuthor;
+}
