68dbf69aa4fc303352bd91f0e2dc24d530868214
beqirajd <dejvis.beqiraj@linksmt.it>
2023-05-13 11:39:28 +0200

    create library event producer component and a sending message to topic method

---
+++ producer/src/main/java/com/example/producer/LibraryEventProducer.java
@@ -0,0 +1,31 @@
+package com.example.producer;
+
+import com.example.domain.LibraryEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class LibraryEventProducer {
+
+    private final KafkaTemplate<Integer, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public LibraryEventProducer(KafkaTemplate<Integer, String> kafkaTemplate, ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    public void sendLibraryEvent(LibraryEvent libraryEvent) throws JsonProcessingException {
+
+        Integer key = libraryEvent.getLibraryEventId();
+        String value = objectMapper.writeValueAsString(libraryEvent);
+
+        kafkaTemplate.sendDefault(key, value);
+    }
+}
