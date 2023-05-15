db709729e7cf1a6ed7c436cf155e4b257692ece9
beqirajd <dejvis.beqiraj@linksmt.it>
2023-05-15 11:37:51 +0200

    configure Acknowledgment mode to MANUAL

--- consumer/src/main/java/com/example/config/ConsumerConfig.java
+++ consumer/src/main/java/com/example/config/ConsumerConfig.java
@@ -1,11 +1,26 @@
 package com.example.config;
 
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
 @EnableKafka
 public class ConsumerConfig {
 
 
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+                                                                                ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory();
+        configurer.configure(factory, kafkaConsumerFactory.getIfAvailable());
+
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL); // offset management
+        return factory;
+    }
 }
---
+++ consumer/src/main/java/com/example/consumer/LibraryEventConsumerManual.java
@@ -0,0 +1,20 @@
+package com.example.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.AcknowledgingMessageListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class LibraryEventConsumerManual implements AcknowledgingMessageListener<Integer, String> {
+
+    @KafkaListener(topics = {"library-events"})
+    @Override
+    public void onMessage(ConsumerRecord<Integer, String> consumerRecord, Acknowledgment acknowledgment) {
+        log.info("Consumer Record: {}", consumerRecord);
+        acknowledgment.acknowledge();
+    }
+}
