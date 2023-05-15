aaeb524a912f3db103484220e63d930c1a953113
beqirajd <dejvis.beqiraj@linksmt.it>
2023-05-15 16:13:55 +0200

    configure default error handler

--- consumer/src/main/java/com/example/config/ConsumerConfig.java
+++ consumer/src/main/java/com/example/config/ConsumerConfig.java
@@ -1,26 +1,26 @@
 package com.example.config;
 
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
 
 @Configuration
 @EnableKafka
 public class ConsumerConfig {
 
-
-   /* @Bean
-    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
-                                                                                ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
-        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory();
-        configurer.configure(factory, kafkaConsumerFactory.getIfAvailable());
-
-        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL); // offset management
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<Integer, String> kafkaListenerContainerFactory(ConsumerFactory<Integer, String> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Integer, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setCommonErrorHandler(errorHandler());
         return factory;
-    }*/
+    }
+
+    private DefaultErrorHandler errorHandler() {
+        return new DefaultErrorHandler(new FixedBackOff(1000L, 2L));
+    }
 }
