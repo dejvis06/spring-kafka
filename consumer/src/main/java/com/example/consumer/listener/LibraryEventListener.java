cd6096fb9979216cb589d7e6aabb5f346c9b2376
beqirajd <dejvis.beqiraj@linksmt.it>
2023-05-15 16:22:05 +0200

    create library event listener

---
+++ consumer/src/main/java/com/example/consumer/listener/LibraryEventListener.java
@@ -0,0 +1,23 @@
+package com.example.consumer.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.listener.RetryListener;
+
+@Slf4j
+public class LibraryEventListener implements RetryListener {
+    @Override
+    public void failedDelivery(ConsumerRecord<?, ?> record, Exception ex, int i) {
+        log.error("Failed Delivery for: {}, error: {}", record, ex);
+    }
+
+    @Override
+    public void recovered(ConsumerRecord<?, ?> record, Exception ex) {
+        log.info("Recovered record: {}, error: {}", record, ex);
+    }
+
+    @Override
+    public void recoveryFailed(ConsumerRecord<?, ?> record, Exception original, Exception failure) {
+        log.info("Recovery failed for: {}, error: {}", record, failure);
+    }
+}
