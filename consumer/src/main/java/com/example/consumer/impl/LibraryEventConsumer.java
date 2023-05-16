043d1ef6b738f918647d0e7abb28f9599a54d2b4
beqirajd <dejvis.beqiraj@linksmt.it>
2023-05-16 11:27:28 +0200

    add condition

--- consumer/src/main/java/com/example/consumer/impl/LibraryEventConsumer.java
+++ consumer/src/main/java/com/example/consumer/impl/LibraryEventConsumer.java
@@ -1,6 +1,9 @@
 package com.example.consumer.impl;
 
 import com.example.consumer.Consumer;
+import com.example.domain.LibraryEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -12,7 +15,11 @@ public class LibraryEventConsumer implements Consumer<Integer, String> {
 
     @KafkaListener(topics = {"library-events"})
     @Override
-    public void consume(ConsumerRecord<Integer, String> consumerRecord) {
+    public void consume(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
+
+        LibraryEvent libraryEvent = new ObjectMapper().readValue(consumerRecord.value(), LibraryEvent.class);
+        if (libraryEvent.getLibraryEventId() == null)
+            throw new IllegalArgumentException("Library Event ID cannot be null!");
         log.info("Consumer Record: {}", consumerRecord);
     }
 }
