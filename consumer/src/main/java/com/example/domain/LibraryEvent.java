e51493656810b9a7dd35de8deb9b6449b3bbbfb9
beqirajd <dejvis.beqiraj@linksmt.it>
2023-05-16 11:27:17 +0200

    add domain models

---
+++ consumer/src/main/java/com/example/domain/LibraryEvent.java
@@ -0,0 +1,23 @@
+package com.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LibraryEvent {
+
+    @NotNull
+    private Integer libraryEventId;
+
+    @NotNull
+    @Valid
+    private Book book;
+}
