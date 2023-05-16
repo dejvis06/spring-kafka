6d0acd4d7c66e154e88ffaebd1c4fdeee317a983
beqirajd <dejvis.beqiraj@linksmt.it>
2023-05-16 11:40:50 +0200

    configure retryable and dlt handling

--- consumer/src/main/java/com/example/config/ConsumerConfig.java
+++ consumer/src/main/java/com/example/config/ConsumerConfig.java
@@ -1,18 +1,29 @@
 package com.example.config;
 
+import com.example.common.exceptions.RetryableException;
 import com.example.consumer.listener.LibraryEventListener;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.TopicPartition;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.ExponentialBackOffWithMaxRetries;
-import org.springframework.util.backoff.FixedBackOff;
 
 @Configuration
 @EnableKafka
+@Slf4j
 public class ConsumerConfig {
+
+    public static final String LIBRARY_EVENTS_RETRY = "library-events.RETRY";
+    public static final String LIBRARY_EVENTS_DLT = "library-events.DLT";
+    @Autowired
+    KafkaTemplate<String, Integer> kafkaTemplate;
 
     @Bean
     ConcurrentKafkaListenerContainerFactory<Integer, String> kafkaListenerContainerFactory(ConsumerFactory<Integer, String> consumerFactory) {
@@ -29,9 +40,21 @@ public class ConsumerConfig {
         exponentialBackOffWithMaxRetries.setMultiplier(2.0);
         exponentialBackOffWithMaxRetries.setMaxInterval(2_000L);
 
-        DefaultErrorHandler errorHandler = new DefaultErrorHandler(exponentialBackOffWithMaxRetries);
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(deadLetterPublishingRecoverer(), exponentialBackOffWithMaxRetries);
         errorHandler.setRetryListeners(new LibraryEventListener());
         errorHandler.addNotRetryableExceptions(IllegalArgumentException.class);
         return errorHandler;
+    }
+
+    DeadLetterPublishingRecoverer deadLetterPublishingRecoverer() {
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate,
+                (r, e) -> {
+                    if (e instanceof RetryableException) {
+                        return new TopicPartition(r.topic() + LIBRARY_EVENTS_RETRY, r.partition());
+                    } else {
+                        return new TopicPartition(r.topic() + LIBRARY_EVENTS_DLT, r.partition());
+                    }
+                });
+        return recoverer;
     }
 }
